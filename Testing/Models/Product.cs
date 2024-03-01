736f9e9cc93310cb84726fadc58732c1d6eaa350
Jon <jonathon_rylatt@yahoo.com>
2024-03-01 11:13:45 +0800

    added 'add product' functionality

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Testing.Models
 {
     public class Product
     {
-        public int ProductId { get; set; }
+        public int ProductID { get; set; }
         public string Name { get; set; }
-        public double Price {  get; set; }
-        public int CategoryID {  get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
         public int OnSale { get; set; }
-        public int StockLevel {  get; set; }
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
